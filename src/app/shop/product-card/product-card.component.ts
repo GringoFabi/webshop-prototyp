b460c622cef2c349483a42d66557c16b50cfa203
fabian.muehlenbein <fabian15121998@googlemail.com>
2021-07-08 16:03:53 +0200

    feat: add image upload (without saving)

--- src/app/shop/product-card/product-card.component.ts
+++ src/app/shop/product-card/product-card.component.ts
@@ -10,14 +10,33 @@ export class ProductCardComponent implements OnInit {
   @Input('product') product: any
   @Output('remove') remove = new EventEmitter<any>();
 
+  numbers: number[];
+
   constructor( ) {
+    this.numbers = Array(5).fill(0).map((x,i)=>i);
   }
 
   ngOnInit(): void {
+    this.product.imageURLs = []
+    console.log(this.product);
   }
 
   removeProduct(product: any) {
     this.remove.emit(product);
   }
 
+  fileChangeEvent(event: any) {
+    const fileData = <File>event.target.files[0];
+    this.addPreview(fileData)
+  }
+
+  private addPreview(fileData: File) {
+    const reader = new FileReader();
+    let imageUrl: string | ArrayBuffer | null = '';
+    reader.readAsDataURL(fileData);
+    reader.onload = (_event) => {
+      imageUrl = reader.result;
+      this.product.imageURLs.push(imageUrl);
+    }
+  }
 }
